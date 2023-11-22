22e0092452e5aae59ccacc9933d8de88f1ab1cb9
david <367013672@qq.com>
2023-11-22 10:54:29 +0800

    fix:构造函数允许Config对象

--- src/PrinterHelper.php
+++ src/PrinterHelper.php
@@ -2,6 +2,7 @@
 
 namespace Ledc\PrinterSdk;
 
+use Closure;
 use Exception;
 
 /**
@@ -11,18 +12,34 @@ use Exception;
 final class PrinterHelper
 {
     /**
+     * 获取配置的回调
+     * @var Closure|callable
+     */
+    protected static $config = null;
+
+    /**
      * @var array<string, Printer>
      */
     protected static $instances = [];
 
     /**
+     * 设置获取配置的回调函数
+     * @param Closure|callable $callable 可以是函数、匿名函数、箭头函数、对象方法、静态方法等
+     * @return void
+     */
+    public static function setConfig($callable)
+    {
+        self::$config = $callable;
+    }
+
+    /**
      * 单例调用
-     * @param array $config
      * @return Printer
      * @throws Exception
      */
-    public static function getInstance(array $config): Printer
+    public static function getInstance(): Printer
     {
+        $config = call_user_func(self::$config);
         $app_id = (new Config($config))->getAppId();
         if (!isset(self::$instances[$app_id])) {
             self::$instances[$app_id] = new Printer($config);
